52653b09df20f2cdc5244aeaaf5b1bf529006cbc
Craig Re <car.code240z@gmail.com>
2015-10-27 09:46:00 -0500

    simple simon revisions

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,56 +1,56 @@
 
 'use strict';
 
-var simonSequence = [];
+var simonArray = [];
 var i = 0;
 
 $('.start').click(function(event) {
-    simonSequence = [];
+    simonArray = [];
     getRandomButton();
     startGame();
 });
 
 $('.button').click(function(event) {
-    var userSelection = event.currentTarget.attributes[1].value;
-    console.log("The user chose to click on button" +  userSelection);
-    var userChoice = $(this).attr('data-num');
-    compareResults(userChoice);
+    var playerSelection = event.currentTarget.attributes[1].value;
+    var playerChoice = $(this).attr('data-num');
+    console.log("The player chose to click on button" +  playerChoice);
+    compareResults(playerChoice);
 });
 
 function startGame() {
     var i = 0;
     var intervalId = setInterval(function() {
-        animateButton(simonSequence[i]);
+        animateButton(simonArray[i]);
         i += 1;
-        if(i >= simonSequence.length) {
+        if(i >= simonArray.length) {
             clearInterval(intervalId);
         }
-    },1000);
+    },750);
 }
 
 function getRandomButton() {
     var randomNumber = Math.floor((Math.random() * 4) + 1);
-    simonSequence.push(randomNumber);
-    console.log("The following value is the simonSequence array.")
-    console.log(simonSequence);
+    simonArray.push(randomNumber);
+    console.log("The following value is the simonArray.")
+    console.log(simonArray);
 }
 
 function animateButton(num) {
     $('[data-num = "' + num + '"]').fadeOut();
     setTimeout(function() {
         $('[data-num = "' + num + '"]').fadeIn();
-    },750);
+    },250);
 
 }
 
-function compareResults(userChoice) {
-    console.log("The user clicked the buttom with the data-num " + userChoice);
-    if (simonSequence[i] != userChoice) {
+function compareResults(playerChoice) {
+    console.log("The player clicked the buttom with the data-num " + playerChoice);
+    if (simonArray[i] != playerChoice) {
         gameOver();
-    } else if(simonSequence[i] == userChoice && simonSequence.length - 1 == i) {
+    } else if(simonArray[i] == playerChoice && simonArray.length - 1 == i) {
         i = 0;
         simonTurn();
-    } else if(simonSequence[i] == userChoice) {
+    } else if(simonArray[i] == playerChoice) {
         i += 1;
     }
 }
@@ -58,11 +58,11 @@ function compareResults(userChoice) {
 function simonTurn() {
     getRandomButton();
     startGame();
-    $('.start').text('Start Game: ' + simonSequence.length);
+    $('.start').text('Start Game: ' + simonArray.length);
 }
 
 function gameOver() {
-    simonSequence = [];
+    simonArray = [];
     alert('Game Over!');
     location.reload(); 
 }
